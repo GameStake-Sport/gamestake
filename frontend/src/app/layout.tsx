c65c43c8bcad1b79ed66ee389128db7681098f93
Humberto Trejos <htrejosmorales@gmail.com>
2024-09-03 12:01:07 -0600

    feat(frontend/src/app):Add UserDashboard to the layout file.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import UserDashboard from '../../components/UserDashboard';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <UserDashboard />
+        {children}
+      </body>
     </html>
   );
 }
