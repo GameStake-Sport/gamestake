c70b40688a5b84acde066d509b96a2b6f5409a25
Jean Pool Cruz <jcruzcespedes72@yahoo.com>
2024-09-06 23:06:28 -0600

    index page was deleted

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
 
+        {/* <Header />
+        <Hero />
+        <Features /> */}
+      </div>
     </main>
   );
-}
+};
