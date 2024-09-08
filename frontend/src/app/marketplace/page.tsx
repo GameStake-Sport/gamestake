1daaf001c210267c6ee3970723f82ef85a50ed1d
Ivan <cortes.ivan353@gmail.com>
2024-09-07 22:19:50 -0600

    history

---
+++ frontend/src/app/marketplace/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+const products = [
+    {
+      id: 1,
+      name: "NFT Pack",
+      description: "A pack of 3 random football NFTs.",
+      price: 100, // en puntos
+      image: "images/pack.png",
+    },
+    {
+      id: 2,
+      name: "NFT Pack",
+      description: "A pack of 3 random Boxers NFTs.",
+      price: 100, // en puntos
+      image: "images/pack.png",
+    },
+    {
+        id: 3,
+        name: "NFT Pack",
+        description: "A pack of 3 random Boxers NFTs.",
+        price: 100, // en puntos
+        image: "images/pack.png",
+      },
+];
+
+import { useEffect,useState } from 'react';
+import { useBetting } from '@/hooks/useBetting';
+import { Toaster, toast } from 'sonner'
+
+
+export default function Marketplace() {
+    const { connectContract, redeemPointsForPack } = useBetting();
+    const [loading, setLoading] = useState(false); // Estado de carga
+    const [nftsDelivered, setNftsDelivered] = useState(false); // Estado para manejar la entrega de NFTs
+
+
+
+    // Conectar al contrato cuando se carga el componente
+    useEffect(() => {
+      connectContract();
+    }, []);
+
+      // Manejar la compra de un producto
+  const handleBuy = async () => {
+    setLoading(true); // Mostrar loader
+    setNftsDelivered(false); // Resetear el estado de entrega de NFTs
+    try {
+      // Aquí pasar la dirección del contrato de GameStakeNFT para redimir el pack
+            // Llamar a la función del contrato para redimir puntos
+      await redeemPointsForPack();
+
+      setLoading(false); // Quitar el loader cuando la transacción se confirme
+      setNftsDelivered(true); // Mostrar que los NFTs fueron entregados
+      toast.success('Compra realizada con éxito!'); // Mostrar notificación de
+    } catch (error) {
+      setLoading(false); // Quitar el loader en caso de error
+      toast.error('Error al realizar la compra: ' + (error as Error).message); // Mostrar notificación de error
+      alert('Error al realizar la compra: ' + (error as Error).message);
+    }
+  };
+
+
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen  p-10">
+        <h1 className="text-4xl font-bold  mb-10 text-white">Marketplace</h1>
+        {loading && (
+        <div className="flex items-center justify-center space-x-2">
+            <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-transparent border-blue-600 rounded-full"></div>
+            <span className="text-blue-600 font-semibold">Procesando la compra...</span>
+        </div>
+        )}
+
+        <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-5xl w-full ${loading ? 'opacity-50 pointer-events-none' : ''}`}>
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white shadow-md rounded-lg p-6 flex flex-col items-center text-center"
+            >
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-full h-40 object-cover rounded-md mb-4 p h-fit"
+              />
+              <h2 className="text-2xl font-semibold text-gray-800 mb-2">{product.name}</h2>
+              <p className="text-gray-600 mb-4">{product.description}</p>
+              <p className="text-xl font-bold text-gray-700 mb-6">Price: {product.price} points</p>
+              <button
+                onClick={() => handleBuy()}
+                className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition-colors"
+              >
+                Comprar
+              </button>
+              <Toaster />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+}
---
+++ frontend/src/hooks/useBetting.ts
@@ -0,0 +1,479 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import { Web3Provider } from '@ethersproject/providers';
+
+interface BetResult {
+  matchId: number;
+  result: number;
+  amount: string;
+}
+
+interface ContractAPI {
+  placeBet: (matchId: number, result: number, amount: string) => Promise<void>;
+  redeemPointsForPack: () => Promise<void>;
+  connectContract: () => Promise<void>;
+}
+
+const contractABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "matchId",
+        "type": "uint256"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "bettor",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "enum FootballBetting.MatchResult",
+        "name": "result",
+        "type": "uint8"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "BetPlaced",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "matchId",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "internalType": "enum FootballBetting.MatchResult",
+        "name": "result",
+        "type": "uint8"
+      }
+    ],
+    "name": "MatchResolved",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "PointsAdded",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "PointsDeducted",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "PointsRedeemed",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "addOwner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "addPoints_owner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "matchId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "startTime",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "endTime",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "payFactor",
+        "type": "uint256"
+      }
+    ],
+    "name": "createMatch",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "matchIds",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "matches",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "matchId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "startTime",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "endTime",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "isResolved",
+        "type": "bool"
+      },
+      {
+        "internalType": "uint256",
+        "name": "payFactor",
+        "type": "uint256"
+      },
+      {
+        "internalType": "enum FootballBetting.MatchResult",
+        "name": "result",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "ownerCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "owners",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "packPricePoints",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "matchId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "enum FootballBetting.MatchResult",
+        "name": "result",
+        "type": "uint8"
+      }
+    ],
+    "name": "placeBet",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "points",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "gameStakeNFTContract",
+        "type": "address"
+      }
+    ],
+    "name": "redeemPointsForPack",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "ownerToRemove",
+        "type": "address"
+      }
+    ],
+    "name": "removeOwner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "matchId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "enum FootballBetting.MatchResult",
+        "name": "result",
+        "type": "uint8"
+      }
+    ],
+    "name": "resolveMatch",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_newPrice",
+        "type": "uint256"
+      }
+    ],
+    "name": "setPackPricePoints_owner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+const contractAddress = '0xf82Ac7d308951E85af01f6698FeE201c24030CB9'; // Reemplaza con la dirección de tu contrato desplegado
+
+export const useBetting = (): ContractAPI => {
+  const [contract, setContract] = useState<ethers.Contract | null>(null);
+
+  // Función para conectar con el contrato
+  const connectContract = async () => {
+    if (typeof window !== 'undefined' && (window as any).ethereum) {
+      try {
+        const provider = new Web3Provider((window as any).ethereum);
+        await provider.send('eth_requestAccounts', []); // Solicita permisos a MetaMask
+        const signer = provider.getSigner();
+        const footballBettingContract = new ethers.Contract(contractAddress, contractABI, signer);
+        setContract(footballBettingContract);
+        console.log('Contrato conectado:', footballBettingContract, signer);
+      } catch (error) {
+        console.error('Error al conectar con MetaMask:', error);
+      }
+    } else {
+      console.error('MetaMask no está instalado.');
+    }
+  };
+
+  // Función para realizar una apuesta
+  const placeBet = async (matchId: number, result: number, amount: string) => {
+    if (!contract) {
+      console.error('Contrato no conectado.');
+      return;
+    }
+    try {
+      const weiAmount = ethers.parseUnits('0.00000000000001', 'ether'); // Aquí especificamos 'ether' para convertir ETH a Wei
+
+      console.log('Realizando apuesta...', weiAmount);
+      const tx = await contract.placeBet(matchId, result, { value: weiAmount });
+      
+      return tx;
+    } catch (error) {
+      console.error('Error al realizar la apuesta:', error);
+    }
+  };
+
+  const redeemPointsForPack = async () => {
+    if (!contract) {
+      console.error('Contrato no conectado.');
+      return;
+    }
+
+    const gameStakeNFTContract = '0x561A4E166C68e11019aaB142B2511513678E2611';
+
+    try {
+      const tx = await contract.redeemPointsForPack(gameStakeNFTContract);
+      console.log('Pack redimido con éxito!');
+      return tx;
+    } catch (error) {
+      console.error('Error al redimir el pack:', error);
+    }
+  };
+
+  return {
+    connectContract,
+    placeBet,
+    redeemPointsForPack
+  };
+};
